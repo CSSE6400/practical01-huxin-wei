b1d0b2118b91da37a78adab1fbf9779387b53333
huxin-wei <54523798+huxin-wei@users.noreply.github.com>
2023-03-01 14:24:54 +0000

    PUT 201

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -41,7 +41,7 @@ def create_todo():
 
 @api.route('/todos/<int:id>', methods=['PUT'])
 def update_todo(id):
-      return jsonify({
+      return jsonify([{
      "id": id,
      "title": "Watch CSSE6400 Lecture",
      "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
@@ -49,7 +49,7 @@ def update_todo(id):
      "deadline_at": "2023-02-27T00:00:00",
      "created_at": "2023-02-20T00:00:00",
      "updated_at": "2023-02-20T00:00:00"
-     })
+     },201])
 
 @api.route('/todos/<int:id>', methods=['DELETE'])
 def delete_todo(id):
